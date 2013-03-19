d3489a5007896c38805ee9e09ca31f8c2491a52f
Tom Wilson <tom@jackhq.com>
2013-03-19 11:46:37 -0400

    confirmed all tests passing

--- server.js
+++ server.js
@@ -1,28 +1,69 @@
+// ~ cloudq3 ~
+//
+// A simple message queue server.
+//
+// cloudq3 uses redis for storage and has a very simple api
+// 
+// Add to Queue
+// POST http://example.com/:queue
+//
+// Get oldest msg in the queue
+// GET http://example.com/:queue
+//
+// Delete message
+// DELETE http://example.com/:id
+//
+// Get Stats
+// GET http://example.com/
+//
+//
+// require block, pull in dependencies
 var restify = require('restify');
 var Thug = require('thug');
 var redisThug = require('thug-redis');
 var redis = require('redis');
-var client = redis.createClient();
-//var config = require('./config');
+
+// start of module, take config argument
 module.exports = function(config) {
+  // if no config create a config object
+  if (!config) { config = { namespace: 'cloudq3'}; }
+  // if no redis conn info, create empty obj
   if (!config.redis) { config.redis = {}; }
+  // copy namespace to redis object for thug-redis
   config.redis.namespace = config.namespace;
+  // create redis client
+  //var client = redis.createClient(config.redis);
+  var client = redis.createClient();
+  // create redisThug object
   var db = redisThug(config.redis);
 
+  // create Thug Model
   var Job = new Thug({
     filters: { 
       beforeWrite: [db.counter('id')]
     },
     methods: {
+      // post job to redis and queue
       post: function(job, cb) {
+        // save job in redis
         this.set('new', job, function(err, job) {
-          if (err) { return res.send(500, err); }
+          if (err) { return cb(err); }
           // set job expiration...
           client.expire(config.namespace + ':' + job.id, config.expire);
+          // add to the end of the queue
           client.rpush('queues:' + job.queue, job.id, function(err, result) {
             if (err) { return cb(err); }
             cb(null, job);
           });
+        });
+      },
+      // grab oldest message in the queue
+      grab: function(queue, cb) {
+        var self = this;
+        client.lpop('queues:' + queue, function(err, id) {
+          if (err) { return cb(err); }
+          if (!id) { return cb(null); }
+          self.get(id, function(job) { cb(null, job); });
         });
       }
     }
@@ -55,7 +96,9 @@ module.exports = function(config) {
   });
 
   server.get('/', function(req, res, next) {
-    res.send('Welcome to Cloudq 3.0');
+    // get queues and counts, and provide back
+    // in json or plaintext, based on the req.content-type
+    res.send('~ Cloudq3 ~');
     return next();
   });
 
@@ -68,14 +111,12 @@ module.exports = function(config) {
     });
   });
 
-  // GET Job
+  // Grab Job
   server.get('/:queue', function(req, res, next) {
-    client.lpop('queues:' + req.params.queue, function(err, id) {
-      if (!id) { return res.send(200); }
-      Job.get(id, function(job) {
-        res.send(job);
-        return next();
-      });
+    Job.grab(req.params.queue, function(err, job) {
+      if (err) { res.send(500, err); }
+      res.send(job);
+      return next();
     });
   });
 
